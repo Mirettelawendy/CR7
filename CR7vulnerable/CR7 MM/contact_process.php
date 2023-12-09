4114c8220ba96b34d26652e3d952a86e014c1a52
Mirettelawendy <114480067+Mirettelawendy@users.noreply.github.com>
2023-12-09 15:36:41 +0200

    Add files via upload

---
+++ CR7vulnerable/CR7 MM/contact_process.php
@@ -0,0 +1,33 @@
+<?php
+// Establish a connection to the database
+$servername = "127.0.0.1";  // Replace with your database server name
+$username = "root";         // Replace with your database username
+$password = "";             // Replace with your database password
+$dbname = "contact";  // Replace with your database name
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Process form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $message = mysqli_real_escape_string($conn, $_POST['message']);
+
+    // Insert data into the database
+    $sql = "INSERT INTO contact_messages (name, email, message) VALUES ('$name', '$email', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Close the database connection
+$conn->close();
+?>
